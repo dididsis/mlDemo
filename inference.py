7385c99c0d126d3e1eca77aee371e920e9e230c1
amtg5 <amtg5dwp@gmail.com>
2025-01-25 02:44:45 +0900

    t

--- inference.py
+++ inference.py
@@ -48,14 +48,14 @@ args.eval_interval = 1
 config = get_config(args)
 
 num_classes = 2
-model = SwinUnet(config, img_size=224, num_classes=num_classes)
 model_weights_path = "best_model.pth"
 
 device= torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 state_dict = torch.load(model_weights_path)
+model = SwinUnet(config, img_size=224, num_classes=num_classes)
 model.load_state_dict(state_dict=state_dict)
-model.to(device)
+model = model.to(device)
 model.eval()
 
 transform=transforms.Compose([
